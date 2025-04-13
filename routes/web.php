e88e88aa6f4423fb04c0cb9ece378b480d25c147
abelll22 <abelhermawan22@gmail.com>
2025-04-13 20:08:37 +0700

    Hapus komentar tidak perlu di route create

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BarangController;
 
 Route::get('/barangs', [BarangController::class, 'index']);
-Route::get('/barangs/create', [BarangController::class, 'create'])->name('barangs.create'); 
+Route::get('/barangs/create', [BarangController::class, 'create']); 
 Route::post('/barangs', [BarangController::class, 'store'])->name('barangs.store');
 Route::get('/barangs/{id}', [BarangController::class, 'show']);
 Route::get('/barangs/{id}/edit', [BarangController::class, 'edit']);
