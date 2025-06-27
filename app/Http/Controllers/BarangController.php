597e6a95df4fff6169381c19c36d141d246ab3ab
abelll22 <abelhermawan22@gmail.com>
2025-06-27 10:45:46 +0700

    Initial commit praktikum cms

--- app/Http/Controllers/BarangController.php
+++ app/Http/Controllers/BarangController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Barang;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\Log;
+
 
 class BarangController extends Controller
 {
@@ -47,7 +49,7 @@ class BarangController extends Controller
     }
 
     public function searchByName(Request $request)
-    {
+{
     $request->validate([
         'title' => 'required'
     ]);
@@ -55,12 +57,25 @@ class BarangController extends Controller
     try {
         $barang = Barang::where('title', $request->title)->firstOrFail();
 
-        // Kalau barang ditemukan → langsung mengarahkan ke halaman detail
+        // LOG INFO jika ditemukan
+        Log::info('Barang ditemukan melalui pencarian.', [
+            'title_input' => $request->title,
+            'barang_id' => $barang->id,
+            'waktu' => now()
+        ]);
+
         return redirect()->route('barangs.show', $barang->id);
 
     } catch (ModelNotFoundException $e) {
-        // Kalau barang tidak ditemukan → Langsung menampilkan pesan error
+        // LOG ERROR jika tidak ditemukan
+        Log::error('Pencarian gagal: Barang tidak ditemukan', [
+            'title_input' => $request->title,
+            'file' => $e->getFile(),
+            'line' => $e->getLine(),
+            'waktu' => now()
+        ]);
+
         return redirect()->back()->withInput()->with('error', 'Barang dengan nama tersebut tidak ditemukan!');
     }
-    }
+}
 }
